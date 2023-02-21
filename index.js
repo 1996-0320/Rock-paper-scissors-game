3f6facec393bf5e55e7a8f93c23a793b0c010f2a
1996-0320 <103819718+1996-0320@users.noreply.github.com>
2023-02-21 11:42:48 +0800

    Update index.js
    change for loop

--- index.js
+++ index.js
@@ -7,31 +7,24 @@ computerSelection=signArray[randomNumber];
   return computerSelection;
 }
 
-//compare the playerselection & computerselection
-
-var playerSelection=prompt("choose rock or paper or scissors pls");
-getComputerChoice(); 
 
 function playRound(playerSelection,computerSelection){
+var playerSelection=prompt("choose rock or paper or scissors pls");
   
   if(playerSelection==computerSelection){ 
     return("equal with computer!");
   } else if( playerSelection=="rock" && computerSelection=="paper" || playerSelection=="paper" && computerSelection=="scissors" || playerSelection=="scissors" && computerSelection=="rock"){
-   // rock win s; paper win rock; s win paper 
     return("You lose!"+ computerSelection+" beat "+playerSelection);
   } else { 
     return("You Win!"+ playerSelection+" beat "+computerSelection);
   } 
 }
-// const playerSelection="rock";
-// const computerSelection=getComputerChoice();
-// console.log(playRound(playerSelection,computerSelection));
 
 function game(){
   for(let i=1; i<=5; i++){
+  getComputerChoice(); 
   playRound(playerSelection,computerSelection);
   console.log(playRound(playerSelection,computerSelection));
   }
 }
 game();
-
