2a2ee09bfd2f7b4526d1516045a958c4bcff800d
1996-0320 <103819718+1996-0320@users.noreply.github.com>
2023-02-21 11:37:12 +0800

    Update index.js
    add game function

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 var signArray=["rock","paper","scissors"];
-var randomChoice="";
+var computerSelection="";
+
 function getComputerChoice(){
 var randomNumber=Math.floor(Math.random()*signArray.length);
-randomChoice=signArray[randomNumber];
-  return randomChoice;
+computerSelection=signArray[randomNumber];
+  return computerSelection;
 }
-getComputerChoice();
 
 //compare the playerselection & computerselection
 
-
 var playerSelection=prompt("choose rock or paper or scissors pls");
-var computerSelection=randomChoice;
-//var playResult="";
+getComputerChoice(); 
+
 function playRound(playerSelection,computerSelection){
+  
   if(playerSelection==computerSelection){ 
-   return("equal with computer!")；
-  } else if( playerSelection=="rock" && computerSelection=="paper" || playerSelection=="paper" && computerSelection="scissors" || playerSelection="scissors" && computerSelection="rock"){
+    return("equal with computer!");
+  } else if( playerSelection=="rock" && computerSelection=="paper" || playerSelection=="paper" && computerSelection=="scissors" || playerSelection=="scissors" && computerSelection=="rock"){
    // rock win s; paper win rock; s win paper 
     return("You lose!"+ computerSelection+" beat "+playerSelection);
   } else { 
     return("You Win!"+ playerSelection+" beat "+computerSelection);
-  }
-  return playResult;
+  } 
 }
+// const playerSelection="rock";
+// const computerSelection=getComputerChoice();
+// console.log(playRound(playerSelection,computerSelection));
+
+function game(){
+  for(let i=1; i<=5; i++){
+  playRound(playerSelection,computerSelection);
+  console.log(playRound(playerSelection,computerSelection));
+  }
+}
+game();
+
